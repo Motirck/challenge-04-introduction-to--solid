6b86c7fd3cc01b5eb86e2b5b8ebf3fdefcd6bdc6
Ricardo Alves Paula <ricardo.alves.p001@gmail.com>
2021-07-16 18:24:56 -0300

    Verify duplicate email

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -1,4 +1,6 @@
 import { User } from '../../model/User';
+import { ErrorHandler } from '../../../../handlers/ErrorHandler';
+
 import { IUsersRepository } from '../../repositories/IUsersRepository';
 
 interface IRequest {
@@ -10,6 +12,19 @@ class CreateUserUseCase {
     constructor(private usersRepository: IUsersRepository) {}
 
     execute({ name, email }: IRequest): User {
+        const emailAlreadyExists = this.usersRepository.findByEmail(email);
+
+        if (emailAlreadyExists) {
+            const err = {
+                name: 'CreateUserFailed',
+                message: 'Informed email is already in use.',
+                statusCode: 400,
+                description: 'Informed email is already in use. Inform a diferent one.',
+            };
+
+            throw new ErrorHandler(err);
+        }
+
         const user = this.usersRepository.create({ name, email });
         return user;
     }
